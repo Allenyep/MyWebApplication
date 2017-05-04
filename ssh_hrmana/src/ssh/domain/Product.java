faf68eca8ae634ffab341e24d6834c22d3cfd680
Allenyep <736304034@qq.com>
2017-05-04 23:03:09 +0800

    ssh搭建hr管理系统demo

---
+++ ssh_hrmana/src/ssh/domain/Product.java
@@ -0,0 +1,34 @@
+package ssh.domain;
+
+/**
+ * Created by allen on 2017/5/4.
+ */
+public class Product {
+    private int pid;
+    private String pname;
+    private double price;
+
+    public int getPid() {
+        return pid;
+    }
+
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+
+    public String getPname() {
+        return pname;
+    }
+
+    public void setPname(String pname) {
+        this.pname = pname;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
---
+++ ssh_hrmana/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: allen
+  Date: 2017/5/4
+  Time: 21:39
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
